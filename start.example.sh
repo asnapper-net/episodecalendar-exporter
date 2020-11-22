bad335b3371487b9d1b82ed77e466e674f07439e
Matthias Löffel <matthias.loeffel@gmail.com>
2020-11-22 01:24:29 +0100

    gh docker actions

--- start.example.sh
+++ start.example.sh
@@ -1,9 +1,9 @@
 # Set DEBUG to "" to disable logging
 export DEBUG="epcal:*"
 # User email address
-export EPCAL_EMAIL="foo@example.com"
+export EPCAL_EMAIL="matthias.loeffel@gmail.com"
 # Base64 encoded user password
 # TODO: implement better password encoding
-export EPCAL_PASS="SGVsbG8sIHdvcmxkIQ=="
+export EPCAL_PASS="VGFwIG9uIGEgY2xpcCB0byBwYXN0ZSBpdCBpbiB0aGUgdGV4dCBib3gu"
 
 node index.js
